98603db5629be776dd429a94e4b8324e9509fb81
Yusuf <kaymazyusuf88@gmail.com>
2025-06-13 14:52:18 +0300

    feat: add jwt and roles

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth";
+import { jwtDecode } from "jwt-decode";
+import NextAuth, { Session } from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
 
 const handler = NextAuth({
@@ -15,9 +16,42 @@ const handler = NextAuth({
       },
     }),
   ],
+
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+   async jwt({ token, account, user }) {
+  if (account && account.id_token) {
+
+    const idToken = account.id_token;
+    const decode_jwt = jwtDecode(idToken) as any
+
+    const roles = decode_jwt["https://localhost:3000/roles"] || [];
+    token.role = roles[0] || "user";
+    token.accessToken = account.access_token;
+    token.userId = user?.id;
+  }
+  return token;
+},
+
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.userId;
+      session.user.role = token.role;
+      return session;
+    },
+
     async redirect({ url, baseUrl }) {
+
       if (url.startsWith("/")) return `${baseUrl}${url}`;
       else if (new URL(url).origin === baseUrl) return url;
       return `${baseUrl}/dashboard`;
