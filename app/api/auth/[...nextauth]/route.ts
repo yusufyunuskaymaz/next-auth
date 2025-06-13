90dae612a3747f0c65decdd62dbd66cee409afa4
Yusuf <kaymazyusuf88@gmail.com>
2025-06-13 10:47:00 +0300

    feat: add auth0 keys to nextauth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+const handler = NextAuth({
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: process.env.AUTH0_ISSUER,
+      authorization: {
+        params: {
+          prompt: "login",
+          scope: "openid email profile",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return `${baseUrl}/dashboard`;
+    },
+  },
+});
+
+export default handler;
+
+export { handler as GET, handler as POST };
