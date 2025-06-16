9a02e13aba932169d4d09f5a9d9d482be1bd292e
Yusuf <kaymazyusuf88@gmail.com>
2025-06-16 14:06:59 +0300

    fix: redirect base path / to login or dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      dsa
-      {/* <Login /> */}
-    </div>
-  );
+  const { data } = useSession();
+
+  if (data?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
 }
