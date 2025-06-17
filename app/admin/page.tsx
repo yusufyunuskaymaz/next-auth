ce79158d8c2059717a0c832d97502caf57457fd1
Yusuf <kaymazyusuf88@gmail.com>
2025-06-17 15:09:55 +0300

    style: page height and color

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      admin page
+     <div className="h-[95vh] bg-gray-900">
+      <div className="h-full flex items-center justify-center">
+        <p>admin page</p>
+      </div>
     </div>
   )
 }
--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-gray-900'>
-      editor page
+     <div className="h-[95vh] bg-gray-900">
+      <div className="h-full flex items-center justify-center">
+        <p>editor page</p>
+      </div>
     </div>
   )
 }
